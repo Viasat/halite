05703bad16b969621d76143276d8f3430a92b47f
Matt McGill <matthew.mcgill@viasat.com>
2022-03-21 16:48:17 -0400

    halite: add back inadvertently removed tests
    [This commit was extracted from another repo]

--- test/jibe/test_halite.clj
+++ test/jibe/test_halite.clj
@@ -41,7 +41,20 @@
     {:$type :ws/C$v1 :xs []} :ws/C$v1
     {:$type :ws/C$v1 :xs [1 2 3]} :ws/C$v1
     [{:$type :ws2/B$v1 :s "bar"}] [:Vec :ws2/B$v1]
-    {:$type :ws/D$v1 :xss [[]]} :ws/D$v1))
+    {:$type :ws/D$v1 :xss [[]]} :ws/D$v1)
+
+  (are [expr err-msg]
+      (thrown-with-msg? ExceptionInfo err-msg (halite/type-check tenv expr))
+
+    {} #"instance literal must have :\$type field"
+    {:$type "foo"} #"expected namespaced keyword as value of :\$type"
+    {:$type :bar} #"expected namespaced keyword as value of :\$type"
+    {:$type :foo/bar} #"resource spec not found"
+    {:$type :ws/A$v1} #"missing required variables"
+    {:$type :ws/A$v1 :x 1 :y 1 :c {:$type :ws2/B$v1 :s "foo"}} #"value of :y has wrong type"
+    {:$type :ws/A$v1 :x 1 :y false :c {:$type :ws2/B$v1 :s 12}} #"value of :s has wrong type"
+    {:$type :ws2/B$v1 :s "foo" :foo "bar"} #"variables not defined on spec"
+    {:$type :ws/C$v1 :xs [1 "two"]} #"value of :xs has wrong type"))
 
 (deftest literal-eval-tests
   (let [env (assoc tenv :bindings {} :refinesTo {})]
@@ -65,7 +78,14 @@
     '(+ 1 2) :Integer
     '(+ (- 3 2) (* 4 5)) :Integer
     '(and (< 1 2) (> (+ 5 6) 90) (or true (<= 1 4))) :Boolean
-    '(Cardinality [true true false]) :Integer))
+    '(Cardinality [true true false]) :Integer)
+
+  (are [expr err-msg]
+      (thrown-with-msg? ExceptionInfo err-msg (halite/type-check tenv expr))
+
+    '(foo) #"function 'foo' not found"
+    '(+ 1 "two") #"no matching signature for '\+'"
+    '(+ 1) #"no matching signature for '\+'"))
 
 (deftest application-eval-tests
   (let [env (assoc tenv :bindings {} :refinesTo {})]
