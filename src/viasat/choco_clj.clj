4ee4eb87ecabf2cf0a0aa03626e7626b8f774304
Matt McGill <matthew.mcgill@viasat.com>
2022-04-10 14:29:53 -0400

    choco-clj: simple front-end to Choco solver
    [This commit was extracted from another repo]

---
+++ src/viasat/choco_clj.clj
@@ -0,0 +1,162 @@
+;; Copyright (c) 2022 Viasat, Inc.
+;; Licensed under the MIT license
+
+(ns viasat.choco-clj
+  (:require [schema.core :as s])
+  (:import [org.chocosolver.solver Model Solver]
+           [org.chocosolver.solver.variables Variable BoolVar IntVar]
+           [org.chocosolver.solver.constraints Constraint]
+           [org.chocosolver.solver.expression.discrete.arithmetic ArExpression ArExpression$IntPrimitive]
+           [org.chocosolver.solver.expression.discrete.relational ReExpression]
+           [org.chocosolver.solver.propagation PropagationEngine]
+           [org.chocosolver.util ESat]))
+
+(set! *warn-on-reflection* true)
+
+(s/defschema ChocoVarType
+  (s/cond-pre
+   (s/enum :Int :Bool)
+   #{s/Int}))
+
+(defn- bool-type? [var-type] (= var-type :Bool))
+(defn- int-type? [var-type] (or (= var-type :Int) (set? var-type)))
+
+(s/defschema ChocoSpec
+  {:vars {s/Symbol ChocoVarType}
+   :constraints #{s/Any}})
+
+(defn- make-var [^Model m [lb ub] [var var-type]]
+  [var
+   [(cond
+      (= var-type :Int) (.intVar m (name var) lb ub true)
+      (= var-type :Bool) (.boolVar m (name var))
+      (set? var-type) (.intVar m (int-array var-type))
+      :else (throw (ex-info (format "Unrecognized var type: '%s'" (pr-str var-type)) {:var-type var-type})))
+    var-type]])
+
+(defn- arithmetic ^"[Lorg.chocosolver.solver.expression.discrete.arithmetic.ArExpression;"
+  [args]
+  (into-array ArExpression args))
+
+(defn- relational ^"[Lorg.chocosolver.solver.expression.discrete.relational.ReExpression;"
+  [args]
+  (into-array ReExpression args))
+
+(defn- make-expr [^Model m vars form]
+  (cond
+    (true? form) (ArExpression$IntPrimitive. 1 m)
+    (false? form) (ArExpression$IntPrimitive. 0 m)
+    (symbol? form) (get vars form)
+    (int? form) (ArExpression$IntPrimitive. form m)
+    (list? form) (let [op (first form)]
+                   (cond
+                     ;; (- x y z) => (- (- x y) z)
+                     (and (= op '-) (< 3 (count form)))
+                     (make-expr m vars (apply list '- (apply list (take 3 form)) (drop 3 form)))
+
+                     (= op 'dec) (make-expr m vars (list '- (second form) 1))
+                     (= op 'inc) (make-expr m vars (list '+ (second form) 1))
+
+                     (= op 'not=) (make-expr m vars (list 'not (apply list '= (rest form))))
+
+                     (= op 'let)
+                     (let [[bindings body] (rest form)]
+                       (if (empty? bindings)
+                         (make-expr m vars body)
+                         (let [[var expr & bindings] bindings]
+                           (make-expr m (assoc vars var (make-expr m vars expr)) (list 'let bindings body)))))
+
+                     :else
+                     (let [[arg1 & other-args] (mapv (partial make-expr m vars) (rest form))]
+                       (condp = op
+                         '+ (.add ^ArExpression arg1 (arithmetic other-args))
+                         '- (.sub ^ArExpression arg1 ^ArExpression (first other-args))
+                         '* (.mul ^ArExpression arg1 (arithmetic other-args))
+                         '< (.lt ^ArExpression arg1 ^ArExpression (first other-args))
+                         '<= (.le ^ArExpression arg1 ^ArExpression (first other-args))
+                         '> (.gt ^ArExpression arg1 ^ArExpression (first other-args))
+                         '>= (.ge ^ArExpression arg1 ^ArExpression (first other-args))
+                         '= (.eq ^ArExpression arg1 (arithmetic other-args))
+                         'and (.and ^ReExpression arg1 (relational other-args))
+                         'or (.or ^ReExpression arg1 (relational other-args))
+                         'not (.not ^ReExpression arg1)
+                         '=> (.imp ^ReExpression arg1 ^ReExpression (first other-args))
+                         'div (.div ^ArExpression arg1 ^ArExpression (first other-args))
+                         'mod (.mod ^ArExpression arg1 ^ArExpression (first other-args))
+                         'expt (.pow ^ArExpression arg1 ^ArExpression (first other-args))
+                         'abs (.abs ^ArExpression arg1)
+                         'if (let [[then else] other-args]
+                               (if (instance? ReExpression then)
+                                 (.and (.imp ^ReExpression arg1 ^ReExpression then)
+                                       (relational [(.imp (.not ^ReExpression arg1) ^ReExpression else)]))
+                                 (.ift ^ReExpression arg1 ^ArExpression then ^ArExpression else)))
+                         (throw (ex-info (format "Unsupported operator '%s'" (pr-str op)) {:form form}))))))
+    :else (throw (ex-info (format "Unsupported constraint: '%s'" (pr-str form)) {:form form}))))
+
+(defn- bool-var-as-expr ^ReExpression
+  [^ReExpression expr]
+  (if (instance? BoolVar expr)
+    (.eq ^BoolVar expr (ArExpression$IntPrimitive. 1 (.getModel expr)))
+    expr))
+
+(defn- make-constraint [^Model m vars form]
+  (let [^ReExpression expr (bool-var-as-expr (make-expr m vars form))
+        constraint (.decompose expr)]
+    (.post constraint)
+    {:form expr :constraint constraint}))
+
+(s/defschema ChocoModel
+  {:model Model
+   :vars {s/Symbol [(s/one Variable :var) ChocoVarType]}
+   :constraints [{:form s/Any
+                  :constraint Constraint}]})
+
+(def ^:dynamic *default-int-bounds* [-1000 1000])
+
+(s/defn ^:private make-model :- ChocoModel
+  [spec :- ChocoSpec]
+  (let [{:keys [default-int-bounds]} spec
+        m (Model.)
+        vars (->> spec :vars (map (partial make-var m *default-int-bounds*)) (into {}))
+        vars' (update-vals vars first)]
+    {:model m
+     :vars vars
+     :constraints (->> spec :constraints (mapv (partial make-constraint m vars')))}))
+
+(s/defschema VarBound
+  (s/cond-pre
+   s/Int
+   s/Bool
+   #{(s/cond-pre s/Int s/Bool)}
+   [(s/one s/Int :lower) (s/one s/Int :upper)]))
+
+(s/defschema VarBounds
+  {s/Symbol VarBound})
+
+(defn- extract-bound
+  [[^Variable v var-type]]
+  (cond
+    (bool-type? var-type) (if (.isInstantiated v)
+                            (= ESat/TRUE (.getBooleanValue ^BoolVar v))
+                            #{true false})
+    (int-type? var-type) (if (.isInstantiated v)
+                           (.getValue ^IntVar v)
+                           (if (.hasEnumeratedDomain ^IntVar v)
+                             (set v)
+                             [(.getLB ^IntVar v) (.getUB ^IntVar v)]))))
+
+(s/defn propagate :- VarBounds
+  [spec :- ChocoSpec]
+  (let [{:keys [^Model model vars]} (make-model spec)]
+    (.. model getSolver propagate)
+    (update-vals vars extract-bound)))
+
+(comment
+
+  (let [spec '{:vars {x :Int, y :Int, b :Bool}
+               :constraints [(< (dec y) x)
+                             (=> b (< x (+ y 1)))]}]
+    (doseq [extra '[[] [(= y 24)] [(= b true)] [(= y 24) (= b true)]]]
+      (prn (propagate (update spec :constraints into extra)))))
+
+  )
---
+++ test/viasat/test_choco_clj.clj
@@ -0,0 +1,86 @@
+;; Copyright (c) 2022 Viasat, Inc.
+;; Licensed under the MIT license
+
+(ns viasat.test-choco-clj
+  (:require [schema.test]
+            [viasat.choco-clj :as choco-clj])
+  (:use clojure.test))
+
+(use-fixtures :once schema.test/validate-schemas)
+
+(deftest test-int-enum
+  (let [spec '{:vars {n #{1 2}
+                      x :Int}
+               :constraints
+               #{(=> (= 1 n) (< x 10))
+                 (=> (= 2 n) (> x 5))}}]
+    (binding [choco-clj/*default-int-bounds* [0 100]]
+      (are [extra-constraints bounds]
+          (= bounds (choco-clj/propagate
+                     (update spec :constraints into extra-constraints)))
+
+        [] '{n #{1 2}, x [0 100]}
+        '[(= x 20)] '{x 20, n 2}))))
+
+(deftest test-int-var-does-not-get-bool-bounds
+  (is (= '{n #{0 1}} (choco-clj/propagate '{:vars {n #{0 1}} :constraints #{}}))))
+
+(deftest test-abstract-translation-strategy
+  ;; An abstract variable expands into a type variable and variables for each
+  ;; concrete spec; for each concrete spec, the variables
+  ;; must be satisfied unconditionally.
+  ;; References to the abstract variable should be conditional on
+  ;; the type var.
+  ;; A stand-alone propagation pass should be done for each spec,
+  ;; and specs that are obviously unsatisfiable should be excluded
+  ;; from the analysis.
+  (let [senv '{:A {:spec-vars {:n :Integer}
+                   :abstract? true
+                   :constraints [["a1" (and (<= 0 n) (<= n 10))]]}
+
+               :X {:spec-vars {:a :A :x :Int}
+                   :constraints [["x1" (= x (get* (refine-to a :A) :n))]]}
+
+               :B {:spec-vars {:b :Integer}
+                   :constraints [["b1" (< b 4)]]
+                   :refines-to {:A {:clauses [["" {:n b}]]}}}
+               :C {:spec-vars {:c :Integer}
+                   :constraints [["c1" (< 2 c)]]
+                   :refines-to {:A {:clauses [["" {:n c}]]}}}}
+
+        alt1 '{:vars {x :Int
+                      a?B|b :Int
+                      a?C|c :Int
+                      a?type #{1 2}}
+               :constraints
+               #{(< a?B|b 4)
+                 (let [n a?B|b]
+                   (and (<= 0 n) (<= n 10)))
+                 (< 2 a?C|c)
+                 (let [n a?C|c]
+                   (and (<= 0 n) (<= n 10)))
+                 (= x (if (= a?type 1) a?B|b a?C|c))
+                 }}
+        alt2 '{:vars {x :Int
+                      a?B|b :Int
+                      a?C|c :Int
+                      a?type #{1 2}}
+               :constraints
+               #{(let [$1 (= a?type 1)
+                       $2 (if $1 a?B|b a?C|c)]
+                   (and (< a?B|b 4)
+                        (and (<= 0 a?B|b) (<= a?B|b 10))
+                        (< 2 a?C|c)
+                        (and (<= 0 a?C|c) (<= a?C|c 10))
+                        (= x $2)))}}
+        spec alt2]
+
+    (binding [choco-clj/*default-int-bounds* [-100 100]]
+      (are [extra bounds]
+          (= bounds (select-keys (choco-clj/propagate (update spec :constraints into extra)) (keys bounds)))
+
+        '[]             '{a?type #{1 2}, x [-100 100]}
+        '[(= x 3)]      '{a?type #{1 2}, a?B|b [0 3], a?C|c [3 10]}
+        '[(= x 2)]      '{a?type 1, a?B|b 2}
+        '[(= a?type 2)] '{x [3 10]}))
+    ))
