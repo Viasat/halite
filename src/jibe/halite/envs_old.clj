d5be4d0b6774a2d3289d2c54c36d4039b6603ef9
David McNeil <david.mcneil@viasat.com>
2022-05-24 18:53:14 -0500

    halite: fork halite to avoid having to change propagation right now
    [This commit was extracted from another repo]

---
+++ src/jibe/halite/envs_old.clj
@@ -0,0 +1,114 @@
+;; Copyright (c) 2022 Viasat, Inc.
+;; Licensed under the MIT license
+
+(ns jibe.halite.envs-old
+  "Halite spec, type, and eval environment abstractions."
+  (:require [jibe.halite.types-old :refer [BareKeyword BareSymbol NamespacedKeyword HaliteType spec-type?]]
+            [schema.core :as s]))
+
+(set! *warn-on-reflection* true)
+
+(s/defschema Refinement
+  {:expr s/Any
+   (s/optional-key :inverted?) s/Bool})
+
+(s/defschema SpecInfo
+  {:spec-vars {BareKeyword HaliteType}
+   :constraints [[(s/one s/Str :name) (s/one s/Any :expr)]]
+   :refines-to {NamespacedKeyword Refinement}
+   (s/optional-key :abstract?) s/Bool})
+
+(defprotocol SpecEnv
+  (lookup-spec* [self spec-id]))
+
+(s/defn lookup-spec :- (s/maybe SpecInfo)
+  "Look up the spec with the given id in the given type environment, returning variable type information.
+  Returns nil when the spec is not found."
+  [senv :- (s/protocol SpecEnv), spec-id :- NamespacedKeyword]
+  (lookup-spec* senv spec-id))
+
+(deftype SpecEnvImpl [spec-info-map]
+  SpecEnv
+  (lookup-spec* [self spec-id] (spec-info-map spec-id)))
+
+(s/defn spec-env :- (s/protocol SpecEnv)
+  [spec-info-map :- {NamespacedKeyword SpecInfo}]
+  (->SpecEnvImpl spec-info-map))
+
+(defprotocol TypeEnv
+  (scope* [self])
+  (extend-scope* [self sym t]))
+
+(s/defn scope :- {BareSymbol HaliteType}
+  "The scope of the current type environment."
+  [tenv :- (s/protocol TypeEnv)]
+  (scope* tenv))
+
+(s/defn extend-scope :- (s/protocol TypeEnv)
+  "Produce a new type environment, extending the current scope by mapping the given symbol to the given type."
+  [tenv :- (s/protocol TypeEnv), sym :- BareSymbol, t :- HaliteType]
+  (extend-scope* tenv sym t))
+
+(defprotocol Env
+  (bindings* [self])
+  (bind* [self sym value]))
+
+(s/defn bindings :- {BareSymbol s/Any}
+  "The bindings of the current environment."
+  [env :- (s/protocol Env)]
+  (bindings* env))
+
+(s/defn bind :- (s/protocol Env)
+  "The environment produced by extending env with sym mapped to value."
+  [env :- (s/protocol Env), sym :- BareSymbol, value :- s/Any]
+  (bind* env sym value))
+
+(deftype TypeEnvImpl [scope]
+  TypeEnv
+  (scope* [self] scope)
+  (extend-scope* [self sym t] (TypeEnvImpl. (assoc scope sym t))))
+
+(s/defn type-env :- (s/protocol TypeEnv)
+  [scope :- {BareSymbol HaliteType}]
+  (->TypeEnvImpl scope))
+
+(s/defn substitute-instance-type :- HaliteType
+  "Return the type obtained by substituting :Instance for any occurrence of a spec-id that is abstract in the given spec environment."
+  [senv :- (s/protocol SpecEnv), declared-type :- HaliteType]
+  (cond
+    (spec-type? declared-type) (let [spec-info (lookup-spec senv declared-type)]
+                                 (when (nil? spec-info)
+                                   (throw (ex-info (format "resource spec not found: '%s'" (symbol declared-type))
+                                                   {:type declared-type, #_#_:senv (.-spec-info-map senv)})))
+                                 (if (:abstract? spec-info)
+                                   :Instance
+                                   declared-type))
+    (vector? declared-type) [(first declared-type) (substitute-instance-type senv (second declared-type))]
+    :else declared-type))
+
+(s/defn type-env-from-spec :- (s/protocol TypeEnv)
+  "Return a type environment where spec lookups are delegated to tenv, but the in-scope symbols
+  are the variables of the given resource spec."
+  [senv :- (s/protocol SpecEnv), spec :- SpecInfo]
+  (let [{:keys [spec-vars]} spec]
+    (->TypeEnvImpl
+     (zipmap (map symbol (keys spec-vars))
+             (map (partial substitute-instance-type senv) (vals spec-vars))))))
+
+(deftype EnvImpl [bindings]
+  Env
+  (bindings* [self] bindings)
+  (bind* [self sym value] (EnvImpl. (assoc bindings sym value))))
+
+(s/defn env :- (s/protocol Env)
+  [bindings :- {BareSymbol s/Any}]
+  (->EnvImpl bindings))
+
+(s/defn env-from-inst :- (s/protocol Env)
+  [spec-info :- SpecInfo, inst]
+  (->EnvImpl
+   (reduce
+    (fn [m kw]
+      (assoc m (symbol kw) (if (contains? inst kw) (kw inst) :Unset)))
+    {}
+    (keys (:spec-vars spec-info)))))
