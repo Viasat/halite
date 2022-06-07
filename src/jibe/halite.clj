9be20e35e54e95c5c55c290bb6da1c644e906f5a
David McNeil <david.mcneil@viasat.com>
2022-06-07 08:47:33 -0500

    halite: added functions to compute 'top-level field constraints' from expressions
    [This commit was extracted from another repo]

--- src/jibe/halite.clj
+++ src/jibe/halite.clj
@@ -330,7 +330,60 @@
                       (reduce into #{}))
      (seq? expr) (gather-seq-free-vars context expr)
      (set? expr) (gather-collection-free-vars context expr)
-     (vector? expr) (gather-collection-free-vars context expr))))
+     (vector? expr) (gather-collection-free-vars context expr)
+     :default (throw (ex-info "unexpected expr to gather-free-vars" {:expr expr})))))
+
+;;;;
+
+(defn- simple-value-or-symbol? [expr]
+  (cond
+    (boolean? expr) true
+    (long? expr) true
+    (string? expr) true
+    (symbol? expr) true
+    (map? expr) true
+    (set? expr) true
+    (vector? expr) true
+    :default false))
+
+(defn- sort-tlfc [tlfc]
+  (-> (group-by gather-free-vars tlfc)
+      (update-keys first)
+      (update-vals set)))
+
+(defn- gather-tlfc*
+  [expr]
+  (cond
+    (and (seq? expr)
+         (= 'and (first expr))) (->> expr
+                                     rest
+                                     (map gather-tlfc*)
+                                     (reduce into #{}))
+    (and (seq? expr)
+         (#{'= '< '<= '> '>=} (first expr))) (if (and (= 1 (count (gather-free-vars expr)))
+                                                      (->> (rest expr)
+                                                           (every? simple-value-or-symbol?))
+                                                      (some symbol? (rest expr)))
+                                               #{expr}
+                                               #{})
+
+    (and (seq? expr)
+         (= 'contains? (first expr))) (let [[_ coll e] expr]
+                                        (if (and (= 1 (count (gather-free-vars expr)))
+                                                 (->> (rest expr)
+                                                      (every? simple-value-or-symbol?))
+                                                 (symbol? e))
+                                          #{expr}
+                                          #{}))
+    :default #{}))
+
+(defn gather-tlfc
+  "Produce a map from symbols to sets of all of the 'top-level-field-constraints' contained in the
+  boolean expr. These are expressions that are clauses of the expression which only refer to a
+  single variable, which are of a form that can be understood simply. These are necessary, but not
+  sufficient constraints for the entire expression to evaluate to 'true'."
+  [expr]
+  (->> expr gather-tlfc* sort-tlfc))
 
 ;;;;
 
--- test/jibe/test_halite.clj
+++ test/jibe/test_halite.clj
@@ -1073,4 +1073,97 @@
                  (+ x e))))
     '#{a b c d e}))
 
+(deftest test-gather-tlfc
+  (are [v expected]
+       (= expected
+          (halite/gather-tlfc v))
+
+    true
+    {}
+
+    '(and true false)
+    {}
+
+    '(= x 1)
+    '{x #{(= x 1)}}
+
+    '(= x (+ 1 3))
+    {}
+
+    '(and (= x 1)
+          (= 2 y))
+    '{x #{(= x 1)}
+      y #{(= 2 y)}}
+
+    '(and (and (= x 1)
+               (= 2 y))
+          z)
+    '{x #{(= x 1)}
+      y #{(= 2 y)}}
+
+    '(< x 1)
+    '{x #{(< x 1)}}
+
+    '(< x y)
+    {}
+
+    '(= x [1 2])
+    '{x #{(= x [1 2])}}
+
+    '(= x [1 z])
+    {}
+
+    '(let [y 1]
+       (< x y))
+    {}
+
+    '(< x (let [y 1] y))
+    {}
+
+    '(contains? #{1 2 3} x)
+    '{x #{(contains? #{1 2 3} x)}}
+
+    '(and (contains? #{1 2 3} x)
+          (= y 1)
+          (and (<= z 20)
+               (> z 10)))
+    '{x #{(contains? #{1 2 3} x)}
+      y #{(= y 1)}
+      z #{(<= z 20)
+          (> z 10)}}
+
+    '(and (contains? #{1 2 3} x)
+          (or a b)
+          (and (= y 1)
+               (and (<= z 20)
+                    (> z 10)))
+          q)
+    '{x #{(contains? #{1 2 3} x)}
+      y #{(= y 1)}
+      z #{(<= z 20)
+          (> z 10)}}
+
+    '(or (= x 1) (= x 2))
+    {}
+
+    '(and (contains? #{1 2 3} x)
+          (or a b)
+          (and (c (every? [c [1 2 3]]
+                          (= c 1)))
+               (and true
+                    (and (= z 1))))
+          (and (= y 1)
+               (and (<= z 20)
+                    (> z 10)))
+          q)
+    '{z #{(> z 10) (<= z 20) (= z 1)}
+      x #{(contains? #{1 3 2} x)}
+      y #{(= y 1)}}
+
+    '(= [1 2] [x 2])
+    {}
+
+    '(= #{x 2} #{1 2})
+    {}))
+
 ;; (clojure.test/run-tests)
