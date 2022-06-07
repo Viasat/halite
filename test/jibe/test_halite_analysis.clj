81c5d2bbd55e9cd5d3320a53239c4b74e7f9df1c
David McNeil <david.mcneil@viasat.com>
2022-06-07 10:57:21 -0500

    test-halite-analysis: refactor test setup to make it easier to work with
    [This commit was extracted from another repo]

--- test/jibe/test_halite_analysis.clj
+++ test/jibe/test_halite_analysis.clj
@@ -55,103 +55,92 @@
     '#{a b c d e}))
 
 (deftest test-gather-tlfc
-  (are [v expected expected-sort]
-       (= [expected expected-sort]
+  (are [v x]
+       (= x
           [(halite-analysis/gather-tlfc v)
            (halite-analysis/sort-tlfc (halite-analysis/gather-tlfc v))])
 
-    true
-    true
-    nil
+    true [true
+          nil]
 
-    '(and true false)
-    'true
-    nil
+    '(and true false) ['true
+                       nil]
 
-    '(= x 1)
-    '(= x 1)
-    '{x (= x 1)}
+    '(= x 100) ['(= x 100)
+                '{x (= x 100)}]
 
-    '(= x (+ 1 3))
-    true
-    nil
+    '(= x (+ 200 3)) [true
+                      nil]
 
-    '(and (= x 1)
-          (= 2 y))
-    '(and (= x 1)
-          (= 2 y))
-    '{x (= x 1)
-      y (= 2 y)}
+    '(and (= x 300)
+          (= 2 y)) ['(and (= x 300)
+                          (= 2 y))
+                    '{x (= x 300)
+                      y (= 2 y)}]
 
-    '(and (and (= x 1)
+    '(and (and (= x 400)
                (= 2 y))
-          z)
-    '(and (= x 1)
-          (= 2 y))
-    '{x (= x 1)
-      y (= 2 y)}
+          z) ['(and (= x 400)
+                    (= 2 y))
+              '{x (= x 400)
+                y (= 2 y)}]
 
-    '(< x 1)
-    '(< x 1)
-    '{x (< x 1)}
+    '(< x 500) ['(< x 500)
+                '{x (< x 500)}]
 
-    '(< x y)
-    true
-    nil
+    '(< x y) [true
+              nil]
 
-    '(= x [1 2])
-    '(= x [1 2])
-    '{x (= x [1 2])}
+    '(= x [1 600]) ['(= x [1 600])
+                    '{x (= x [1 600])}]
 
-    '(= x [1 z])
-    true
-    nil
+    '(= x [700 z]) [true
+                    nil]
 
-    '(let [y 1]
-       (< x y))
-    true
-    nil
+    '(let [y 800]
+       (< x y)) [true
+                 nil]
 
-    '(< x (let [y 1] y))
-    true
-    nil
+    '(< x (let [y 900] y)) [true
+                            nil]
 
-    '(contains? #{1 2 3} x)
-    '(contains? #{1 3 2} x)
-    '{x (contains? #{1 3 2} x)}
+    '(contains? #{1000 2 3} x) ['(contains? #{1000 3 2} x)
+                                '{x (contains? #{1000 3 2} x)}]
 
-    '(and (contains? #{1 2 3} x)
+    '(and (contains? #{1100 2 3} x)
           (= y 1)
           (and (<= z 20)
-               (> z 10)))
-    '(and (contains? #{1 3 2} x)
-          (= y 1)
-          (<= z 20)
-          (> z 10))
-    '{x (contains? #{1 3 2} x),
-      y (= y 1),
-      z (and (<= z 20) (> z 10))}
+               (> z 10))) ['(and (contains? #{1100 3 2} x)
+                                 (= y 1)
+                                 (<= z 20)
+                                 (> z 10))
+                           '{x (contains? #{1100 3 2} x),
+                             y (= y 1),
+                             z (and (<= z 20) (> z 10))}]
 
-    '(and (contains? #{1 2 3} x)
+    '(and (contains? #{1200 2 3} x)
           (or a b)
           (and (= y 1)
                (and (<= z 20)
                     (> z 10)))
-          q)
-    '(and
-      (contains? #{1 3 2} x)
-      (= y 1)
-      (<= z 20)
-      (> z 10))
-    '{x (contains? #{1 3 2} x)
-      y (= y 1),
-      z (and (<= z 20) (> z 10))}
+          q) ['(and
+                (contains? #{1200 3 2} x)
+                (= y 1)
+                (<= z 20)
+                (> z 10))
+              '{x (contains? #{1200 3 2} x)
+                y (= y 1),
+                z (and (<= z 20) (> z 10))}]
 
-    '(or (= x 1) (= x 2))
-    '(or (= x 1) (= x 2))
-    '{x (or (= x 1) (= x 2))}
+    '(or (= x 1300) (= x 2)) ['(or (= x 1300) (= x 2))
+                              '{x (or (= x 1300) (= x 2))}]
 
-    '(and (contains? #{1 2 3} x)
+    '(and (or (= x 1310) (= x 2))
+          (= y 3)) ['(and (or (= x 1310) (= x 2)) (= y 3))
+                    '{x (or (= x 1310) (= x 2))
+                      y (= y 3)}]
+
+    '(and (contains? #{1400 2 3} x)
           (or a b)
           (c (every? [c [1 2 3]]
                      (= c 1)))
@@ -159,32 +148,28 @@
           (= y 1)
           (<= z 20)
           (> z 10)
-          q)
-    '(and
-      (contains? #{1 3 2} x)
-      (= z 1)
-      (= y 1)
-      (<= z 20)
-      (> z 10))
-    '{x (contains? #{1 3 2} x)
-      z (and (= z 1) (<= z 20) (> z 10)),
-      y (= y 1)}
+          q) ['(and
+                (contains? #{1400 3 2} x)
+                (= z 1)
+                (= y 1)
+                (<= z 20)
+                (> z 10))
+              '{x (contains? #{1400 3 2} x)
+                z (and (= z 1) (<= z 20) (> z 10)),
+                y (= y 1)}]
 
-    '(= [1 2] [x 2])
-    true
-    nil
+    '(= [1 1500] [x 1500]) [true
+                            nil]
 
-    '(= #{x 2} #{1 2})
-    true
-    nil
+    '(= #{x 1600} #{1 1600}) [true
+                              nil]
 
     '(or (and (>= x 3)
               (< x 12))
          (and (>= x 20)
-              (< x 25)))
-    '(or (and (>= x 3) (< x 12))
-         (and (>= x 20) (< x 25)))
-    '{x (or (and (>= x 3) (< x 12))
-            (and (>= x 20) (< x 25)))}))
+              (< x 1700))) ['(or (and (>= x 3) (< x 12))
+                                 (and (>= x 20) (< x 1700)))
+                            '{x (or (and (>= x 3) (< x 12))
+                                    (and (>= x 20) (< x 1700)))}]))
 
 ;; (run-tests)
