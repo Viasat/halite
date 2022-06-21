aefd1d09bfaf9f23a6a77e225368a13c216b5653
David McNeil <david.mcneil@viasat.com>
2022-06-21 15:38:36 -0500

    jadeite: represent fixed decimal numbers as '#d <string>' like halite does
    [This commit was extracted from another repo]

--- src/jibe/logic/jadeite.clj
+++ src/jibe/logic/jadeite.clj
@@ -99,7 +99,7 @@
                                 (list 'let (mapv toh (drop-last args)) (toh (last args)))
                                 (toh (last args)))
      [[:int & strs]]          (parse-long (apply str strs))
-     [[:dec & strs]]          (fixed/fixed-reader (apply str strs))
+     [[:decimal s]]           (fixed/fixed-reader (edn/read-string (second s)))
      [[:symbol "true"]]       true
      [[:symbol "false"]]      false
      [[:symbol s]]            (unwrap-symbol s)
@@ -170,7 +170,7 @@
 (defn toj [x]
   (cond
     (string? x) (pr-str x)
-    (halite/fixed-decimal? x) (fixed/string-representation x)
+    (halite/fixed-decimal? x) (str "#d" " \"" (fixed/string-representation x) "\"")
     (keyword? x) (typename x)
     (symbol? x) (if (re-find #"[^a-zA-Z0-9./$]" (str x))
                   (str "<" x ">")
