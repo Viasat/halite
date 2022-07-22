49e6f3cad37933979e95541f410b301e456cb031
Matt McGill <matthew.mcgill@viasat.com>
2022-07-22 22:05:24 +0000

    halite: fixpoint can optionally fail after *fixpoint-iteration-limit* iterations
    [This commit was extracted from another repo]

--- src/jibe/halite/transpile/util.clj
+++ src/jibe/halite/transpile/util.clj
@@ -12,9 +12,14 @@
     1 (first clauses)
     (apply list op clauses)))
 
+(def ^:dynamic *fixpoint-iteration-limit* nil)
+
 (defn fixpoint
   "Iterate f on input until (= x (f x))."
   [f input]
-  (loop [x input]
-    (let [x' (f x)]
-      (cond-> x' (not= x' x) recur))))
+  (loop [x input, i 0]
+    (if (and *fixpoint-iteration-limit* (< *fixpoint-iteration-limit* i))
+      (throw (ex-info (format "Failed to reach fixpoint within %d iterations!" *fixpoint-iteration-limit*)
+                      {:input input :current x}))
+      (let [x' (f x)]
+        (cond-> x' (not= x' x) (recur (inc i)))))))
