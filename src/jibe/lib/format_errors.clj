17fefc139e4ae50ce7fa77c162b528fbd3791eb8
David McNeil <david.mcneil@viasat.com>
2022-08-05 16:24:18 -0500

    format-errors: add functions for analyzing error definitions and invocations
    [This commit was extracted from another repo]

--- src/jibe/lib/format_errors.clj
+++ src/jibe/lib/format_errors.clj
@@ -7,11 +7,106 @@
 
 (set! *warn-on-reflection* true)
 
-(def trace-err-definitions? false)
+(def trace-err-defs? false)
+
+(def trace-atom (atom []))
+
+(defn extract-system-name-from-err-id [err-id]
+  (second (re-matches #"([a-z]+)-[a-z-]+" (name err-id))))
+
+(defn update-fields [fields new-fields]
+  (loop [m fields
+         [f & more-f] new-fields]
+    (let [m' (update m f #(if % (inc %) 1))]
+      (if more-f
+        (recur m' more-f)
+        m'))))
+
+(defn analyze-err-defs []
+  (loop [err-defs {}
+         fields {}
+         field-index {}
+         systems {}
+         [t & more-t] @trace-atom]
+    (let [[err-defs' fields' field-index' systems']
+          (condp = (second t)
+            :deferr (let [[ns-name _ err-id message] t]
+                      (when (contains? err-defs err-id)
+                        (throw (ex-info "duplicate err-id" {:err-id err-id})))
+                      [(assoc err-defs err-id {:message message
+                                               :fields #{}})
+                       fields
+                       field-index
+                       (update systems (extract-system-name-from-err-id err-id) #(if % (conj % ns-name) #{ns-name}))])
+
+            :throw-err (let [[ns-name _ err-id data] t
+                             new-fields (if (map? data)
+                                          (set (keys data))
+                                          #{'?})]
+                         [(update-in err-defs [err-id :fields] into new-fields)
+                          (update-fields fields new-fields)
+                          (loop [m field-index
+                                 [f & more-f] new-fields]
+                            (let [m' (update m f #(if % (conj % err-id) #{err-id}))]
+                              (if more-f
+                                (recur m' more-f)
+                                m')))
+                          systems])
+
+            :with-exception-data (let [[ns-name _ data] t
+                                       new-fields (if (map? data)
+                                                    (set (keys data))
+                                                    #{'?})]
+                                   [err-defs
+                                    (update-fields fields new-fields)
+                                    field-index
+                                    systems]))]
+      (if more-t
+        (recur err-defs' fields' field-index' systems' more-t)
+        {:err-defs err-defs'
+         :fields fields'
+         :field-index field-index'
+         :systems systems'}))))
+
+(defn assemble-err-ids []
+  (update-vals (group-by extract-system-name-from-err-id (keys (:err-defs (analyze-err-defs)))) (comp vec sort)))
+
+(defn find-string [x]
+  (cond
+    (string? x) x
+    (and (list? x) (= 'format (first x))) (find-string (second x))
+    :else ""))
+
+(defn assemble-err-messages []
+  (vec (sort-by find-string
+                (map :message (vals (:err-defs (analyze-err-defs)))))))
+
+(defn field-used-by [field]
+  (get (:field-index (analyze-err-defs)) field))
+
+(defn fields-by-frequency []
+  (vec (reverse (sort-by second (:fields (analyze-err-defs))))))
+
+(defn fields-by-name []
+  (vec (sort-by (comp str first) (:fields (analyze-err-defs)))))
+
+(comment
+  (analyze-err-defs)
+
+  (clojure.pprint/pprint (:systems (analyze-err-defs)))
+  (clojure.pprint/pprint (assemble-err-ids))
+  (map prn (assemble-err-messages))
+
+  (clojure.pprint/pprint (fields-by-frequency))
+  (clojure.pprint/pprint (fields-by-name))
+
+  (clojure.pprint/pprint (:field-index (analyze-err-defs)))
+  (clojure.pprint/pprint (field-used-by ...)))
 
 (defmacro deferr [err-id [data-arg] data]
-  (when trace-err-definitions?
-    (prn [:deferr err-id (:message data)]))
+  (when trace-err-defs?
+    (let [t [(str (.name *ns*)) :deferr err-id (:message data)]]
+      (swap! trace-atom conj t)))
   `(defn ~err-id [~data-arg]
      (merge ~data-arg
             ~(merge {:err-id (keyword (name err-id))
@@ -25,14 +120,16 @@
 
 (defmacro throw-err
   ([data]
-   (when trace-err-definitions?
-     (prn [:throw-err (first data) (second data)]))
+   (when trace-err-defs?
+     (let [t [(str (.name *ns*)) :throw-err (first data) (second data)]]
+       (swap! trace-atom conj t)))
    `(let [data# ~data]
       (throw (ex-info (:message data#)
                       (extend-err-data data#)))))
   ([data ex]
-   (when trace-err-definitions?
-     (prn [:throw-err (first data) (second data)]))
+   (when trace-err-defs?
+     (let [t [(str (.name *ns*)) :throw-err (first data) (second data)]]
+       (swap! trace-atom conj t)))
    `(let [data# ~data]
       (throw (ex-info (:message data#)
                       (extend-err-data data#)
@@ -44,8 +141,9 @@
    (let [[message extra-data body] (if (map? (first args))
                                      [nil (first args) (rest args)]
                                      [(first args) (second args) (rest (rest args))])]
-     (when trace-err-definitions?
-       (prn [:with-exception-data extra-data]))
+     (when trace-err-defs?
+       (let [t [(str (.name *ns*)) :with-exception-data extra-data]]
+         (swap! trace-atom conj t)))
      (assert (map? extra-data))
      (if message
        `(try
