b21e78e4768a00e5d4ec49492346b9450fcc6e26
Chouser <chris.houser@viasat.com>
2022-08-22 15:14:42 +0000

    jibe: add load-kaocha
    [This commit was extracted from another repo]

---
+++ test/load_kaocha.clj
@@ -0,0 +1,22 @@
+;; Copyright (c) 2022 Viasat, Inc.
+;; Licensed under the MIT license
+
+(ns load-kaocha
+  (:require [kaocha.repl]
+            [kaocha.stacktrace]))
+
+(defn run [& args]
+  (binding [kaocha.stacktrace/*stacktrace-filters* (conj kaocha.stacktrace/*stacktrace-filters*
+                                                         "kaocha" "nrepl" "load_kaocha")]
+    (apply kaocha.repl/run args)))
+
+(in-ns 'kaocha.report)
+
+(defn print-output [m]
+  (let [output (get-in m [:kaocha/testable :kaocha.plugin.capture-output/output])
+        buffer (get-in m [:kaocha/testable :kaocha.plugin.capture-output/buffer])
+        out (or output (and buffer (capture/read-buffer buffer)))]
+    (when (seq out)
+      (println "───── Test output ───────────────────────────────────────────────────────")
+      (println (str/trim-newline out))
+      (println "─────────────────────────────────────────────────────────────────────────"))))
