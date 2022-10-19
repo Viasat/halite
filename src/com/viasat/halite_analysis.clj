a78b2ed485e82007089af4b90623ba770c65aafc
David McNeil <david.mcneil@viasat.com>
2022-10-19 14:57:38 -0500

    halite: lift parts of halite.type-check up to be part of the public halite API
    [This commit was extracted from another repo]

--- src/com/viasat/halite_analysis.clj
+++ src/com/viasat/halite_analysis.clj
@@ -4,6 +4,7 @@
 (ns com.viasat.halite-analysis
   "Public API for analyzing halite expressions"
   (:require [com.viasat.halite.analysis :as analysis]
+            [com.viasat.halite.type-check :as halite-type-check]
             [potemkin]))
 
 (set! *warn-on-reflection* true)
@@ -12,3 +13,7 @@
  [analysis
   find-spec-refs-but-tail Range Natural gather-referenced-spec-ids gather-free-vars compute-tlfc-map
   replace-free-vars make-fixed-decimal-string encode-fixed-decimals])
+
+(potemkin/import-vars
+ [halite-type-check
+  find-field-accesses])
