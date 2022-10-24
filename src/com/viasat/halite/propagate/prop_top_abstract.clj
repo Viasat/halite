740de7b36923db6b57596dd1012f12e25f2e80f0
David McNeil <david.mcneil@viasat.com>
2022-10-24 13:33:28 -0500

    prop-to-abstract: revert change to support multi-refinements
    [This commit was extracted from another repo]

--- src/com/viasat/halite/propagate/prop_top_abstract.clj
+++ src/com/viasat/halite/propagate/prop_top_abstract.clj
@@ -2,186 +2,32 @@
 ;; Licensed under the MIT license
 
 (ns com.viasat.halite.propagate.prop-top-abstract
-  (:require [clojure.set :as set]
-            [com.viasat.halite.propagate.prop-abstract :as prop-abstract]
+  (:require [com.viasat.halite.propagate.prop-abstract :as prop-abstract]
             [com.viasat.halite.propagate.prop-composition :as prop-composition]
-            [com.viasat.halite.propagate.prop-strings :as prop-strings]
             [com.viasat.halite.transpile.ssa :as ssa :refer [SpecCtx]]
             [schema.core :as s]))
 
 (def ^:private generated-field-name :$x)
 
-;;;;
-
-(defn remove-vals-from-map
-  "Remove entries from the map if applying f to the value produced false."
-  [m f]
-  (->> m
-       (remove (comp f second))
-       (apply concat)
-       (apply hash-map)))
-
-(defn no-empty
-  "Convert empty collection to nil"
-  [coll]
-  (when (seq coll)
-    coll))
-
-(defn no-nil
-  "Remove all nil values from the map"
-  [m]
-  (remove-vals-from-map m nil?))
-
-;;
-
-(s/defn ^:private generate-var-name
-  "Generate a unique variable name that will not collide with the current context."
-  [spec-id]
-  (keyword (str (namespace spec-id)
-                (str "$" (name spec-id)))))
-
 (s/defn ^:private translate-down
   "Convert a top-level bound by expressing it in terms of a new, enclosing spec."
   [generated-spec-id bound]
-  (assoc (zipmap (map generate-var-name (keys (:$refines-to bound)))
-                 (map (fn [[spec-id bound-val]]
-                        {:$refines-to {spec-id bound-val}}) (:$refines-to bound)))
-         :$type generated-spec-id))
-
-(declare combine-bounds)
-
-(s/defn ^:private combine-abstract-$in-bounds
-  [a-in :- prop-abstract/SpecIdToBoundWithRefinesTo
-   b-in :- prop-abstract/SpecIdToBoundWithRefinesTo]
-  (let [common-spec-ids (set/intersection (set (keys a-in))
-                                          (set (keys b-in)))]
-    (when-not (seq common-spec-ids)
-      (prop-strings/throw-contradiction))
-    (merge-with combine-bounds
-                (select-keys a-in common-spec-ids)
-                (select-keys b-in common-spec-ids))))
-
-(s/defn ^:private combine-untyped-spec-bounds
-  [a :- prop-abstract/UntypedSpecBound
-   b :- prop-abstract/UntypedSpecBound]
-  (merge-with combine-bounds a b))
-
-(s/defn ^:private combine-spec-bounds
-  [a :- prop-abstract/SpecBound
-   b :- prop-abstract/SpecBound]
-  (let [{a-type :$type
-         a-refines-to :$refines-to
-         a-in :$in} a
-        {b-type :$type
-         b-refines-to :$refines-to
-         b-in :$in} b]
-    (when (and a-type b-type (not= a-type b-type))
-      (prop-strings/throw-contradiction))
-    (merge (no-nil {:$type (or a-type b-type)
-                    :$refines-to (no-empty
-                                  ;; TODO: consider detecting whether there are any specs that refine to the combined results
-                                  ;; or has that already been accounted for?
-                                  (merge-with combine-untyped-spec-bounds a-refines-to b-refines-to))
-                    ;; TODO: if there is a single common-spec-id, then lift it up to the :$type
-                    :$in (when (or (seq a-in) (seq b-in))
-                           (combine-abstract-$in-bounds a-in b-in))})
-           (combine-untyped-spec-bounds (dissoc a :$in :$if :$type :$refines-to)
-                                        (dissoc b :$in :$if :$type :$refines-to)))))
-
-(s/defn ^:private bound-type
-  [bound :- prop-abstract/Bound]
-  (cond
-    (:$type bound) prop-abstract/SpecBound
-    (or (not (map? bound))
-        (and (contains? bound :$in)
-             (not (map? (:$in bound))))) prop-composition/AtomBound
-    :else prop-abstract/SpecBound))
-
-(s/defn ^:private primitive?
-  [bound :- prop-strings/AtomBound]
-  (or (integer? bound)
-      (boolean? bound)
-      (string? bound)))
-
-(s/defn ^:private atom-bound-object
-  [bound :- prop-strings/AtomBound]
-  (cond
-    (primitive? bound) {:bound {:$in #{bound}}
-                        :bound-type :enum}
-    (#{:Unset :String} bound) {:bound {:$in #{bound}}
-                               :bound-type :enum}
-    (set? (:$in bound)) {:bound bound
-                         :bound-type :enum}
-    (vector? (:$in bound)) {:bound bound
-                            :bound-type :range}
-    :default (throw (ex-info "unknown atom bound" {:bound bound}))))
-
-(s/defn ^:private simplify-enum :- prop-strings/AtomBound
-  [enum :- #{s/Any}]
-  (cond
-    (empty? enum) (prop-strings/throw-contradiction)
-    (= 1 (count enum)) (first enum)
-    :default {:$in enum}))
-
-(s/defn ^:private combine-atom-bounds
-  [a b]
-  (let [bound-objects (sort-by :bound-type (map atom-bound-object [a b]))
-        [x y] (map :bound bound-objects)]
-    (condp = (vec (map :bound-type bound-objects))
-      [:enum :enum] (simplify-enum (set/intersection (:$in x)
-                                                     (:$in y)))
-      [:enum :range] (let [[lower upper unset?] (:$in y)
-                           unsettable? (and
-                                        ; range allows :Unset
-                                        unset?
-                                        ; enum allows :Unset
-                                        (contains? (:$in x) :Unset))
-                           filtered-enum (set (filter #(<= lower % upper) (disj (:$in x)
-                                                                                :Unset)))
-                           filtered-enum (if unsettable?
-                                           (conj filtered-enum :Unset)
-                                           filtered-enum)]
-                       (simplify-enum filtered-enum))
-      [:range :range] (let [[x-lower x-upper x-unset?] (:$in x)
-                            [y-lower y-upper y-unset?] (:$in y)
-                            lower (max x-lower y-lower)
-                            upper (min x-upper y-upper)
-                            unsettable? (and x-unset? y-unset?)]
-                        (cond
-                          (and (> lower upper) unsettable?) :Unset
-                          (> lower upper) (prop-strings/throw-contradiction)
-                          :default {:$in (if unsettable?
-                                           [lower upper :Unset]
-                                           [lower upper])})))))
-
-(s/defn ^:private combine-bounds
-  [a :- prop-abstract/Bound
-   b :- prop-abstract/Bound]
-  (let [bound-types (set (map bound-type [a b]))]
-    (when (> (count bound-types) 1)
-      (throw (ex-info "invalid bound types" {:bound-types bound-types})))
-    (let [bound-type (first bound-types)]
-      (condp = bound-type
-        prop-abstract/SpecBound (combine-spec-bounds a b)
-        prop-composition/AtomBound (combine-atom-bounds a b)))))
+  {:$type generated-spec-id
+   generated-field-name bound})
 
 (s/defn ^:private translate-up
   "Convert the resulting bound by lifting the bound on the fabricated spec's field to be the
   top-level result bound."
-  [bound :- prop-abstract/ConcreteSpecBound2]
-  (let [bounds (-> bound
-                   (dissoc :$type :$refines-to)
-                   vals)]
-    (reduce combine-spec-bounds (first bounds) (rest bounds))))
+  [bound]
+  (generated-field-name bound))
 
-(s/defn ^:private add-specs
+(s/defn ^:private add-spec
   "Fabricate a new spec to hold a field of the required abstract type. Add it to the context."
-  [generated-spec-id sctx refines-to-spec-ids]
+  [generated-spec-id sctx refines-to-spec-id]
   (assoc sctx
          generated-spec-id
          (ssa/spec-to-ssa (ssa/as-spec-env sctx)
-                          {:spec-vars (zipmap (map generate-var-name refines-to-spec-ids)
-                                              refines-to-spec-ids)
+                          {:spec-vars {generated-field-name refines-to-spec-id}
                            :constraints []
                            :refines-to {}})))
 
@@ -199,12 +45,17 @@
     opts :- prop-composition/Opts
     initial-bound :- prop-abstract/SpecBound]
    (let [{:keys [$refines-to $type]} initial-bound]
-     (if $type
-       ;; the bound is a concrete bound, bypass this module's functionality by passing the call straight through
+     (if (or
+          ;; the bound is a concrete bound
+          $type
+          ;; the bound specifies multiple refinements
+          (> (count $refines-to) 1))
+       ;; bypass this module's functionality by passing the call straight through
        (prop-abstract/propagate sctx opts initial-bound)
        ;; if the bound is an abstract bound then perform the transformation to turn it into a bound
        ;; on a field
-       (let [generated-spec-id (generate-spec-id (key (first $refines-to)))]
-         (translate-up (prop-abstract/propagate (add-specs generated-spec-id sctx (keys $refines-to))
+       (let [refines-to-spec-id (key (first $refines-to))
+             generated-spec-id (generate-spec-id refines-to-spec-id)]
+         (translate-up (prop-abstract/propagate (add-spec generated-spec-id sctx refines-to-spec-id)
                                                 opts
                                                 (translate-down generated-spec-id initial-bound))))))))
--- test/com/viasat/halite/propagate/test_prop_top_abstract.clj
+++ test/com/viasat/halite/propagate/test_prop_top_abstract.clj
@@ -27,43 +27,43 @@
                        {:$refines-to {:ws/A {}}}))))
 
 (deftest test-top-level-refines-to-bound-multiple
-  (is (= {:$in {:ws/Q {:$refines-to #:ws{:ws/A {}
-                                         :ws/C {}
-                                         :ws/X {}
-                                         :ws/Y {}}}}
-          :$refines-to {:ws/A {}
-                        :ws/C {}
-                        :ws/X {}
-                        :ws/Y {}}}
-         (hp/propagate '{:ws/A {:abstract? true
-                                :spec-vars {}
-                                :constraints []
-                                :refines-to {}}
+  #_(is (= {:$in {:ws/Q {:$refines-to #:ws{:ws/A {}
+                                           :ws/C {}
+                                           :ws/X {}
+                                           :ws/Y {}}}}
+            :$refines-to {:ws/A {}
+                          :ws/C {}
+                          :ws/X {}
+                          :ws/Y {}}}
+           (hp/propagate '{:ws/A {:abstract? true
+                                  :spec-vars {}
+                                  :constraints []
+                                  :refines-to {}}
 
-                         :ws/B {:spec-vars {}
-                                :constraints []
-                                :refines-to {:ws/A {:expr {:$type :ws/A}}}}
+                           :ws/B {:spec-vars {}
+                                  :constraints []
+                                  :refines-to {:ws/A {:expr {:$type :ws/A}}}}
 
-                         :ws/C {:spec-vars {}
-                                :constraints []
-                                :refines-to {:ws/A {:expr {:$type :ws/A}}}}
+                           :ws/C {:spec-vars {}
+                                  :constraints []
+                                  :refines-to {:ws/A {:expr {:$type :ws/A}}}}
 
-                         :ws/X {:abstract? true
-                                :spec-vars {}
-                                :constraints []
-                                :refines-to {}}
+                           :ws/X {:abstract? true
+                                  :spec-vars {}
+                                  :constraints []
+                                  :refines-to {}}
 
-                         :ws/Y {:spec-vars {}
-                                :constraints []
-                                :refines-to {:ws/X {:expr {:$type :ws/X}}}}
+                           :ws/Y {:spec-vars {}
+                                  :constraints []
+                                  :refines-to {:ws/X {:expr {:$type :ws/X}}}}
 
-                         :ws/Q {:spec-vars {}
-                                :constraints []
-                                :refines-to {:ws/Y {:expr {:$type :ws/Y}}
-                                             :ws/C {:expr {:$type :ws/C}}}}}
+                           :ws/Q {:spec-vars {}
+                                  :constraints []
+                                  :refines-to {:ws/Y {:expr {:$type :ws/Y}}
+                                               :ws/C {:expr {:$type :ws/C}}}}}
 
-                       {:$refines-to {:ws/A {}
-                                      :ws/X {}}}))))
+                         {:$refines-to {:ws/A {}
+                                        :ws/X {}}}))))
 
 (deftest test-top-level-refines-to-bound-multiple-with-fields
   #_(is (= {:$in {:ws/Q {:$refines-to {:ws/Y {:y {:$in [120 130]}}
@@ -105,87 +105,161 @@
                                         :ws/X {:x {:$in [0 1300]}}}}))))
 
 (deftest test-top-level-refines-to-bound-multiple-with-fields-inverted
-  (is (= {:$in {:ws/Q {:q {:$in [120 130]}
-                       :$refines-to {:ws/Y {:y {:$in [120 130]}}
-                                     :ws/B {:b {:$in [120 130]}}
-                                     :ws/X {:x {:$in [120 130]}}
-                                     :ws/A {:a {:$in [120 130]}}}}}
-          :$refines-to {:ws/A {:a {:$in [120 130]}}
-                        :ws/B {:b {:$in [120 130]}}
-                        :ws/Y {:y {:$in [120 130]}}
-                        :ws/X {:x {:$in [120 130]}}}}
-         (hp/propagate '{:ws/A {:abstract? true
-                                :spec-vars {:a "Integer"}
-                                :constraints [["ac" (< a 200)]]
-                                :refines-to {}}
+  #_(is (= {:$in {:ws/Q {:q {:$in [120 130]}
+                         :$refines-to {:ws/Y {:y {:$in [120 130]}}
+                                       :ws/B {:b {:$in [120 130]}}
+                                       :ws/X {:x {:$in [120 130]}}
+                                       :ws/A {:a {:$in [120 130]}}}}}
+            :$refines-to {:ws/A {:a {:$in [120 130]}}
+                          :ws/B {:b {:$in [120 130]}}
+                          :ws/Y {:y {:$in [120 130]}}
+                          :ws/X {:x {:$in [120 130]}}}}
+           (hp/propagate '{:ws/A {:abstract? true
+                                  :spec-vars {:a "Integer"}
+                                  :constraints [["ac" (< a 200)]]
+                                  :refines-to {}}
 
-                         :ws/B {:spec-vars {:b "Integer"}
-                                :constraints []
-                                :refines-to {:ws/A {:expr {:$type :ws/A
-                                                           :a b}}}}
+                           :ws/B {:spec-vars {:b "Integer"}
+                                  :constraints []
+                                  :refines-to {:ws/A {:expr {:$type :ws/A
+                                                             :a b}}}}
 
-                         :ws/X {:abstract? true
-                                :spec-vars {:x "Integer"}
-                                :constraints [["xc" (> x 100)]]
-                                :refines-to {}}
+                           :ws/X {:abstract? true
+                                  :spec-vars {:x "Integer"}
+                                  :constraints [["xc" (> x 100)]]
+                                  :refines-to {}}
 
-                         :ws/Y {:spec-vars {:y "Integer"}
-                                :constraints []
-                                :refines-to {:ws/X {:expr {:$type :ws/X
-                                                           :x y}}}}
+                           :ws/Y {:spec-vars {:y "Integer"}
+                                  :constraints []
+                                  :refines-to {:ws/X {:expr {:$type :ws/X
+                                                             :x y}}}}
 
-                         :ws/Q {:spec-vars {:q "Integer"}
-                                :constraints []
-                                :refines-to {:ws/Y {:expr {:$type :ws/Y
-                                                           :y q}}
-                                             :ws/B {:expr {:$type :ws/B
-                                                           :b q}}}}}
+                           :ws/Q {:spec-vars {:q "Integer"}
+                                  :constraints []
+                                  :refines-to {:ws/Y {:expr {:$type :ws/Y
+                                                             :y q}}
+                                               :ws/B {:expr {:$type :ws/B
+                                                             :b q}}}}}
 
-                       {:$refines-to {:ws/A {:a {:$in [120 130]}}
-                                      :ws/X {:x {:$in [120 130]}}}}))))
+                         {:$refines-to {:ws/A {:a {:$in [120 130]}}
+                                        :ws/X {:x {:$in [120 130]}}}}))))
+
+(deftest test-top-level-refines-to-bound-multiple-with-fields-inverted-merge-maybes
+  #_(is (= {:$in {:ws/Q {:q {:$in [120 130]}
+                         :$refines-to {:ws/Y {:y {:$in [120 130]}}
+                                       :ws/B {:b {:$in [120 130]}}
+                                       :ws/X {:x {:$in [120 130]}}
+                                       :ws/A {:a {:$in [120 130]}}}}}
+            :$refines-to {:ws/A {:a {:$in [120 130]}}
+                          :ws/B {:b {:$in [120 130]}}
+                          :ws/Y {:y {:$in [120 130]}}
+                          :ws/X {:x {:$in [120 130]}}}}
+           (hp/propagate '{:ws/A {:abstract? true
+                                  :spec-vars {:a "Integer"}
+                                  :constraints [["ac" (< a 200)]]
+                                  :refines-to {}}
+
+                           :ws/B {:spec-vars {:b "Integer"}
+                                  :constraints []
+                                  :refines-to {:ws/A {:expr {:$type :ws/A
+                                                             :a b}}}}
+
+                           :ws/X {:abstract? true
+                                  :spec-vars {:x "Integer"}
+                                  :constraints [["xc" (> x 100)]]
+                                  :refines-to {}}
+
+                           :ws/Y {:spec-vars {:y "Integer"}
+                                  :constraints []
+                                  :refines-to {:ws/X {:expr {:$type :ws/X
+                                                             :x y}}}}
+
+                           :ws/Q {:spec-vars {:q "Integer"
+                                              :p [:Maybe :ws/P]}
+                                  :constraints []
+                                  :refines-to {:ws/Y {:expr {:$type :ws/Y
+                                                             :y (if-value p
+                                                                          200
+                                                                          q)}}
+                                               :ws/B {:expr {:$type :ws/B
+                                                             :b q}}}}
+                           :ws/P {:spec-vars {}
+                                  :constraints []
+                                  :refines-to {}}}
+
+                         {:$refines-to {:ws/A {:a {:$in [120 130]}}
+                                        :ws/X {:x {:$in [120 130]}}}}))))
 
 (deftest test-top-level-refines-to-bound-multiple-no-match
-  (is (= :contradiction
-         (try (hp/propagate '{:ws/A {:abstract? true
-                                     :spec-vars {}
-                                     :constraints []
-                                     :refines-to {}}
+  #_(is (= :contradiction
+           (try (hp/propagate '{:ws/A {:abstract? true
+                                       :spec-vars {}
+                                       :constraints []
+                                       :refines-to {}}
 
-                              :ws/B {:spec-vars {}
-                                     :constraints []
-                                     :refines-to {:ws/A {:expr {:$type :ws/A}}}}
+                                :ws/B {:spec-vars {}
+                                       :constraints []
+                                       :refines-to {:ws/A {:expr {:$type :ws/A}}}}
 
-                              :ws/C {:spec-vars {}
-                                     :constraints []
-                                     :refines-to {:ws/A {:expr {:$type :ws/A}}}}
+                                :ws/C {:spec-vars {}
+                                       :constraints []
+                                       :refines-to {:ws/A {:expr {:$type :ws/A}}}}
 
-                              :ws/X {:abstract? true
-                                     :spec-vars {}
-                                     :constraints []
-                                     :refines-to {}}
+                                :ws/X {:abstract? true
+                                       :spec-vars {}
+                                       :constraints []
+                                       :refines-to {}}
 
-                              :ws/Y {:spec-vars {}
-                                     :constraints []
-                                     :refines-to {:ws/X {:expr {:$type :ws/X}}}}}
+                                :ws/Y {:spec-vars {}
+                                       :constraints []
+                                       :refines-to {:ws/X {:expr {:$type :ws/X}}}}}
 
-                            {:$refines-to {:ws/A {}
-                                           :ws/X {}}})
-              (catch ContradictionException _
-                :contradiction))))
+                              {:$refines-to {:ws/A {}
+                                             :ws/X {}}})
+                (catch ContradictionException _
+                  :contradiction))))
 
-  (is (thrown-with-msg? ExceptionInfo #"No concrete specs"
-                        (hp/propagate '{:ws/A {:abstract? true
-                                               :spec-vars {}
-                                               :constraints []
-                                               :refines-to {}}
+  #_(is (thrown-with-msg? ExceptionInfo #"No concrete specs"
+                          (hp/propagate '{:ws/A {:abstract? true
+                                                 :spec-vars {}
+                                                 :constraints []
+                                                 :refines-to {}}
 
-                                        :ws/X {:abstract? true
-                                               :spec-vars {}
-                                               :constraints []
-                                               :refines-to {}}}
+                                          :ws/X {:abstract? true
+                                                 :spec-vars {}
+                                                 :constraints []
+                                                 :refines-to {}}}
 
-                                      {:$refines-to {:ws/A {}
-                                                     :ws/X {}}}))))
+                                        {:$refines-to {:ws/A {}
+                                                       :ws/X {}}}))))
+
+(deftest test-with-maybe-type
+  #_(is (= {:$type :ws/B
+            :$refines-to {:ws/A {:x {:$type [:Maybe :ws/X]
+                                     :y {:$in [-1000 1000]}}}}})
+        (hp/propagate '{:ws/A {:abstract? true
+                               :spec-vars {:x [:Maybe :ws/X]}
+                               :constraints []
+                               :refines-to {}}
+
+                        :ws/B {:spec-vars {:b "Integer"}
+                               :constraints []
+                               :refines-to {:ws/A {:expr {:$type :ws/A
+                                                          :x (when (> b 0)
+                                                               {:$type :ws/A
+                                                                :y b})}}}}
+
+                        :ws/P {:abstract? true
+                               :spec-vars {}
+                               :constraints []
+                               :refines-to {:ws/A {:expr {:$type :ws/A}}}}
+
+                        :ws/X {:spec-vars {:y "Integer"}
+                               :constraints []
+                               :refines-to {}}}
+
+                      {:$refines-to {:ws/A {}
+                                     :ws/P {}}})))
 
 (deftest test-top-abstract-refines-to-concrete
   #_(is (= {:$in #:ws{:B {:$refines-to #:ws{:A {}}}
