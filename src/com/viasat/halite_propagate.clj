a0193faf47c063ae5414b354a7a075ce90d7dc82
Joe Bain <joe.bain@viasat.com>
2023-02-27 17:47:25 -0800

    constrict, tests passing
    [This commit was extracted from another repo]

--- src/com/viasat/halite_propagate.clj
+++ src/com/viasat/halite_propagate.clj
@@ -22,6 +22,16 @@
    (let [senv (var-types/to-halite-spec-env senv)]
      (propagate/propagate senv opts initial-bound))))
 
+(s/defn propagate-halite-senv :- propagate/SpecBound
+  "Call propagate with an senv already formatted for Halite"
+  ([senv :- (s/protocol envs/SpecEnv)
+    initial-bound :- propagate/SpecBound]
+   (propagate-halite-senv senv propagate/default-options initial-bound))
+  ([senv :- (s/protocol envs/SpecEnv)
+    opts :- prop-abstract/Opts
+    initial-bound :- propagate/SpecBound]
+   (propagate/propagate senv opts initial-bound)))
+
 (potemkin/import-vars
  [propagate
   default-options])
