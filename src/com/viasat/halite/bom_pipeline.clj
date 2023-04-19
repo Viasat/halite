f9d581dd30e517e79ddd231f697c04176289249c
David McNeil <david.mcneil@viasat.com>
2023-04-19 09:43:48 -0500

    break the bom-pipeline out of the test
    [This commit was extracted from another repo]

---
+++ src/com/viasat/halite/bom_pipeline.clj
@@ -0,0 +1,72 @@
+;; Copyright (c) 2022,2023 Viasat, Inc.
+;; Licensed under the MIT license
+
+(ns com.viasat.halite.bom-pipeline
+  (:require [com.viasat.halite.bom :as bom]
+            [com.viasat.halite.bom-choco :as bom-choco]
+            [com.viasat.halite.op-add-constraints :as op-add-constraints]
+            [com.viasat.halite.op-add-types :as op-add-types]
+            [com.viasat.halite.op-add-value-fields :as op-add-value-fields]
+            [com.viasat.halite.op-canon :as op-canon]
+            [com.viasat.halite.op-canon-refinements :as op-canon-refinements]
+            [com.viasat.halite.op-canon-up :as op-canon-up]
+            [com.viasat.halite.op-conjoin-spec-bom :as op-conjoin-spec-bom]
+            [com.viasat.halite.op-contradictions :as op-contradictions]
+            [com.viasat.halite.op-ensure-fields :as op-ensure-fields]
+            [com.viasat.halite.op-find-concrete :as op-find-concrete]
+            [com.viasat.halite.op-flower :as op-flower]
+            [com.viasat.halite.op-id :as op-id]
+            [com.viasat.halite.op-inflate :as op-inflate]
+            [com.viasat.halite.op-mandatory :as op-mandatory]
+            [com.viasat.halite.op-push-down-to-concrete :as op-push-down-to-concrete]
+            [com.viasat.halite.op-remove-value-fields :as op-remove-value-fields]
+            [com.viasat.halite.op-strip :as op-strip]
+            [com.viasat.halite.op-syntax-check :as op-syntax-check]
+            [com.viasat.halite.op-type-check :as op-type-check]))
+
+(set! *warn-on-reflection* true)
+
+(defn propagate [spec-env input-bom]
+  (let [expanded-bom (->> input-bom
+                          (op-syntax-check/syntax-check-op spec-env)
+                          (op-type-check/type-check-op spec-env)
+                          op-canon/canon-op
+                          (op-mandatory/mandatory-op spec-env)
+
+                          (op-find-concrete/find-concrete-op spec-env)
+                          op-push-down-to-concrete/push-down-to-concrete-op
+                          (op-canon-refinements/canon-refinements-op spec-env)
+
+                          (op-conjoin-spec-bom/conjoin-spec-bom-op spec-env)
+                          (op-find-concrete/find-concrete-op spec-env)
+                          op-push-down-to-concrete/push-down-to-concrete-op
+                          (op-canon-refinements/canon-refinements-op spec-env)
+                          op-contradictions/bubble-up-contradictions
+                          (op-add-types/add-types-op spec-env)
+                          (op-ensure-fields/ensure-fields-op spec-env)
+                          (op-add-value-fields/add-value-fields-op spec-env)
+                          (op-add-constraints/add-constraints-op spec-env))
+         ;; _# (clojure.pprint/pprint [:bom bom'
+        lowered-bom (->> expanded-bom
+                         op-id/id-op
+                         (op-flower/flower-op spec-env))
+        choco-data (when-not (bom/is-contradiction-bom? lowered-bom)
+                     (->> lowered-bom
+                          bom-choco/bom-to-choco
+                          (bom-choco/paths-to-syms lowered-bom)))
+        propagate-result (when-not (bom/is-contradiction-bom? lowered-bom)
+                           (->> choco-data
+                                (bom-choco/choco-propagate expanded-bom)))
+        result (if (bom/is-contradiction-bom? lowered-bom)
+                 lowered-bom
+                 (->> propagate-result
+                      (bom-choco/propagate-results-to-bounds expanded-bom)
+                      (op-inflate/inflate-op (op-remove-value-fields/remove-value-fields-op spec-env expanded-bom))
+                      (op-remove-value-fields/remove-value-fields-op spec-env)
+                      op-canon-up/canon-up-op
+                      op-strip/strip-op))]
+    {:expanded-bom expanded-bom
+     :lowered-bom lowered-bom
+     :choco-data choco-data
+     :propagate-result propagate-result
+     :result result}))
--- test/com/viasat/halite/test_bom_pipeline.clj
+++ test/com/viasat/halite/test_bom_pipeline.clj
@@ -4,24 +4,16 @@
 (ns com.viasat.halite.test-bom-pipeline
   (:require [clojure.test :refer :all]
             [com.viasat.halite.bom :as bom]
-            [com.viasat.halite.bom-choco :as bom-choco]
-            [com.viasat.halite.op-add-constraints :as op-add-constraints]
+            [com.viasat.halite.bom-pipeline :as bom-pipeline]
             [com.viasat.halite.op-add-types :as op-add-types]
-            [com.viasat.halite.op-add-value-fields :as op-add-value-fields]
             [com.viasat.halite.op-canon :as op-canon]
             [com.viasat.halite.op-canon-refinements :as op-canon-refinements]
             [com.viasat.halite.op-canon-up :as op-canon-up]
             [com.viasat.halite.op-conjoin-spec-bom :as op-conjoin-spec-bom]
             [com.viasat.halite.op-contradictions :as op-contradictions]
-            [com.viasat.halite.op-ensure-fields :as op-ensure-fields]
             [com.viasat.halite.op-find-concrete :as op-find-concrete]
-            [com.viasat.halite.op-flower :as op-flower]
-            [com.viasat.halite.op-id :as op-id]
-            [com.viasat.halite.op-inflate :as op-inflate]
             [com.viasat.halite.op-mandatory :as op-mandatory]
             [com.viasat.halite.op-push-down-to-concrete :as op-push-down-to-concrete]
-            [com.viasat.halite.op-remove-value-fields :as op-remove-value-fields]
-            [com.viasat.halite.op-strip :as op-strip]
             [com.viasat.halite.op-syntax-check :as op-syntax-check]
             [com.viasat.halite.op-type-check :as op-type-check]
             [schema.test]
@@ -194,44 +186,10 @@
          spec-env0# (quote ~spec-env)
          spec-env# (eval spec-env0#)
          bom0# ~bom
-         bom# (->> bom0#
-                   (op-syntax-check/syntax-check-op spec-env#)
-                   (op-type-check/type-check-op spec-env#)
-                   op-canon/canon-op
-                   (op-mandatory/mandatory-op spec-env#)
-
-                   (op-find-concrete/find-concrete-op spec-env#)
-                   op-push-down-to-concrete/push-down-to-concrete-op
-                   (op-canon-refinements/canon-refinements-op spec-env#)
-
-                   (op-conjoin-spec-bom/conjoin-spec-bom-op spec-env#)
-                   (op-find-concrete/find-concrete-op spec-env#)
-                   op-push-down-to-concrete/push-down-to-concrete-op
-                   (op-canon-refinements/canon-refinements-op spec-env#)
-                   op-contradictions/bubble-up-contradictions
-                   (op-add-types/add-types-op spec-env#)
-                   (op-ensure-fields/ensure-fields-op spec-env#)
-                   (op-add-value-fields/add-value-fields-op spec-env#)
-                   (op-add-constraints/add-constraints-op spec-env#))
-         ;; _# (clojure.pprint/pprint [:bom bom#])
-         lowered-bom# (->> bom#
-                           op-id/id-op
-                           (op-flower/flower-op spec-env#))
-         choco-data# (when-not (bom/is-contradiction-bom? lowered-bom#)
-                       (->> lowered-bom#
-                            bom-choco/bom-to-choco
-                            (bom-choco/paths-to-syms lowered-bom#)))
-         propagate-result# (when-not (bom/is-contradiction-bom? lowered-bom#)
-                             (->> choco-data#
-                                  (bom-choco/choco-propagate bom#)))
-         result# (if (bom/is-contradiction-bom? lowered-bom#)
-                   lowered-bom#
-                   (->> propagate-result#
-                        (bom-choco/propagate-results-to-bounds bom#)
-                        (op-inflate/inflate-op (op-remove-value-fields/remove-value-fields-op spec-env# bom#))
-                        (op-remove-value-fields/remove-value-fields-op spec-env#)
-                        op-canon-up/canon-up-op
-                        op-strip/strip-op))
+         {lowered-bom# :lowered-bom
+          choco-data# :choco-data
+          propagate-result# :propagate-result
+          result# :result} (bom-pipeline/propagate spec-env# bom0#)
          test-result-block# (if comment#
                               [lowered-bom# choco-data# result# comment#]
                               [lowered-bom# choco-data# result#])]
