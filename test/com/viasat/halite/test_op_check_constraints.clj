5e8145b3b1783dfea3cc8549c0adcdcde8c23d42
David McNeil <david.mcneil@viasat.com>
2023-08-07 14:32:17 -0500

    test-op-check-constraints: no need for a macro since the assertion is outside of the helper function
    [This commit was extracted from another repo]

--- test/com/viasat/halite/test_op_check_constraints.clj
+++ test/com/viasat/halite/test_op_check_constraints.clj
@@ -15,40 +15,40 @@
 
 (use-fixtures :each fixtures)
 
-(defmacro check [spec-env bom]
-  `(->> ~bom
-        (op-add-constraints/add-constraints-op ~spec-env)
-        (op-check-constraints/check-constraints-op ~spec-env)))
+(defn check-constraints [spec-env bom]
+  (->> bom
+       (op-add-constraints/add-constraints-op spec-env)
+       (op-check-constraints/check-constraints-op spec-env)))
 
 (deftest test-basic
   (is (nil?
-       (check {:ws/A$v1 {:fields {:x :Integer}}}
-              {:$instance-of :ws/A$v1, :x {:$enum #{1 2}}})))
+       (check-constraints {:ws/A$v1 {:fields {:x :Integer}}}
+                          {:$instance-of :ws/A$v1, :x {:$enum #{1 2}}})))
   (is (nil?
-       (check {:ws/A$v1 {:fields {:x :Integer}}}
-              {:$instance-of :ws/A$v1
-               :x 1})))
+       (check-constraints {:ws/A$v1 {:fields {:x :Integer}}}
+                          {:$instance-of :ws/A$v1
+                           :x 1})))
   (is (nil?
-       (check {:ws/A$v1 {:fields {:x :Integer}
-                         :constraints [["valid_x" '(and (> x 0) (< x 10000))]]}}
-              {:$instance-of :ws/A$v1
-               :x 1})))
+       (check-constraints {:ws/A$v1 {:fields {:x :Integer}
+                                     :constraints [["valid_x" '(and (> x 0) (< x 10000))]]}}
+                          {:$instance-of :ws/A$v1
+                           :x 1})))
   (is (= {[] ["valid_x"]}
-         (check {:ws/A$v1 {:fields {:x :Integer}
-                           :constraints [["valid_x" '(and (> x 0) (< x 10000))]]}}
-                {:$instance-of :ws/A$v1
-                 :x -1})))
+         (check-constraints {:ws/A$v1 {:fields {:x :Integer}
+                                       :constraints [["valid_x" '(and (> x 0) (< x 10000))]]}}
+                            {:$instance-of :ws/A$v1
+                             :x -1})))
 
   (is (= {[] ["valid_y"]
           [:a] ["valid_x"]}
-         (check {:ws/A$v1 {:fields {:x :Integer}
-                           :constraints [["valid_x" '(and (> x 0) (< x 10000))]]}
-                 :ws/B$v1 {:fields {:y :Integer
-                                    :a [:Instance :ws/A$v1]}
-                           :constraints [["valid_y" '(< y 0)]]}}
-                {:$instance-of :ws/B$v1
-                 :a {:$instance-of :ws/A$v1
-                     :x -1}
-                 :y 0}))))
+         (check-constraints {:ws/A$v1 {:fields {:x :Integer}
+                                       :constraints [["valid_x" '(and (> x 0) (< x 10000))]]}
+                             :ws/B$v1 {:fields {:y :Integer
+                                                :a [:Instance :ws/A$v1]}
+                                       :constraints [["valid_y" '(< y 0)]]}}
+                            {:$instance-of :ws/B$v1
+                             :a {:$instance-of :ws/A$v1
+                                 :x -1}
+                             :y 0}))))
 
 ;; (run-tests)
